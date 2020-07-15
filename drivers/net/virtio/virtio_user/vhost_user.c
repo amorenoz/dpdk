998e74f4aed6858181e514e3e4a0ded588ae56c2
Adrian Moreno <amorenoz@redhat.com>
2020-07-15 12:38:47 +0200

    net/virtio: add GET_STATUS support to virtio-user
    This patch adds support for VHOST_USER_GET_STATUS request.
    
    Only vhost-user backed is supported for now
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- drivers/net/virtio/virtio_user/vhost_user.c
+++ drivers/net/virtio/virtio_user/vhost_user.c
@@ -277,6 +277,7 @@ vhost_user_sock(struct virtio_user_dev *dev,
 	switch (req) {
 	case VHOST_USER_GET_FEATURES:
 	case VHOST_USER_GET_PROTOCOL_FEATURES:
+	case VHOST_USER_GET_STATUS:
 		need_reply = 1;
 		break;
 
@@ -371,6 +372,7 @@ vhost_user_sock(struct virtio_user_dev *dev,
 
 		switch (req) {
 		case VHOST_USER_GET_FEATURES:
+		case VHOST_USER_GET_STATUS:
 		case VHOST_USER_GET_PROTOCOL_FEATURES:
 			if (msg.size != sizeof(m.payload.u64)) {
 				PMD_DRV_LOG(ERR, "Received bad msg size");
